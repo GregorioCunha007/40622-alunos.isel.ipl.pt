59b3ea21dc521935f9c70954cccd7f353c807427
GregorioCunha007 <40622@alunos.isel.ipl.pt>
2020-03-26 02:52:44 +0000

    Utils class to load json data into lib

---
+++ RiotSimplify/RiotSimplify/Utils.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RiotSimplify
+{
+    public static class Utils
+    {
+        private static string LoadJson(string path)
+        {
+            try
+            {
+                using (StreamReader r = new StreamReader(path))
+                {
+                    if (r == null)
+                    {
+                        throw new Exception("File has no content");
+                    }
+
+                    string json = r.ReadToEnd();
+                    return json;
+                }
+
+            } catch(Exception e)
+            {
+                throw new Exception(string.Format("Error loading from file path {0}", path), e);
+            }
+        }
+        
+        public static long GetSeasonTimestamp(int seasonId)
+        {
+            try
+            {
+                dynamic json = (Array) JsonConvert.DeserializeObject(LoadJson(@"D:\Riot-Simplify\RiotSimplify\RiotSimplify\StaticData\patches.json"));
+
+                foreach (var patch in json.patches)
+                {
+                    string name = patch.name;
+
+                    // Start of season X.1
+                    if (name == seasonId + ".1")
+                    {
+                        return patch.start;
+                    }
+                }
+
+                return 0;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(string.Format("Failed getting timestamp for seasonId {0}", seasonId), e);
+            }
+        }
+    }
+}
