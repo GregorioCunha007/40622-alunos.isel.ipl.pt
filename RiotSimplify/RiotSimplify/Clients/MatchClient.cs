470fc43976bca5a12bcf357454179f7b542286fc
GregorioCunha007 <40622@alunos.isel.ipl.pt>
2020-03-26 02:53:08 +0000

    Started MatchClient

--- RiotSimplify/RiotSimplify/Clients/MatchClient.cs
+++ RiotSimplify/RiotSimplify/Clients/MatchClient.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RiotSimplify.Mappers;
+using Flurl.Http;
 
 namespace RiotSimplify.Clients
 {
     public class MatchClient
     {
+
+        private String _matchListEndpoint = "/lol/match/v4/matchlists/by-account/";
+
+        public String AccountId { get; set; }
+
         internal List<MatchResult> GetMatchesBySeason(int seasonId, Dictionary<string, string> queryStringOptions)
         {
+            if (String.IsNullOrEmpty(AccountId))
+            {
+                throw new Exception("AccountId cannot be null or empty");
+            }
+
+            long seasonTimestamp = Utils.GetSeasonTimestamp(seasonId); 
+
             throw new NotImplementedException();
         }
     }
